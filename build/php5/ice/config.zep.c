2fc145c54822418c21cf15ba909f0a327bf20090
Ice Framework <info@iceframework.org>
2018-09-09 16:34:43 +0000

    php5, 249afb13acd5046adbc436566a15860064bf95e6: regenerate ice sources [ci skip]

--- build/php5/ice/config.zep.c
+++ build/php5/ice/config.zep.c
@@ -13,10 +13,13 @@
 
 #include "kernel/main.h"
 #include "kernel/operators.h"
-#include "kernel/exception.h"
-#include "kernel/object.h"
+#include "kernel/file.h"
 #include "kernel/memory.h"
+#include "kernel/require.h"
+#include "kernel/exception.h"
 #include "kernel/fcall.h"
+#include "kernel/array.h"
+#include "kernel/object.h"
 
 
 /**
@@ -43,49 +46,79 @@ ZEPHIR_INIT_CLASS(Ice_Config) {
  */
 PHP_METHOD(Ice_Config, __construct) {
 
-	HashTable *_1, *_4$$8;
-	HashPosition _0, _3$$8;
-	zend_bool hasNumericKey = 0;
-	zephir_fcall_cache_entry *_7 = NULL;
+	zval *_3$$5;
+	HashTable *_7, *_10$$11;
+	HashPosition _6, _9$$11;
+	zend_bool hasNumericKey = 0, _0;
+	zephir_fcall_cache_entry *_5 = NULL, *_13 = NULL;
 	zend_long ZEPHIR_LAST_CALL_STATUS;
-	zval *data = NULL, *key = NULL, *value = NULL, *subkey = NULL, *subvalue = NULL, **_2, **_5$$8, *_6$$12 = NULL;
+	zval *data = NULL, *key = NULL, *value = NULL, *subkey = NULL, *subvalue = NULL, **_8, *_1$$4 = NULL, *_2$$5, *_4$$5, **_11$$11, *_12$$15 = NULL;
 
 	ZEPHIR_MM_GROW();
 	zephir_fetch_params(1, 0, 1, &data);
 
 	if (!data) {
-		data = ZEPHIR_GLOBAL(global_null);
+		ZEPHIR_CPY_WRT(data, ZEPHIR_GLOBAL(global_null));
+	} else {
+		ZEPHIR_SEPARATE_PARAM(data);
 	}
 
 
+	_0 = Z_TYPE_P(data) == IS_STRING;
+	if (_0) {
+		_0 = !ZEPHIR_IS_STRING(data, "");
+	}
+	if (_0) {
+		if ((zephir_file_exists(data TSRMLS_CC) == SUCCESS)) {
+			ZEPHIR_OBSERVE_OR_NULLIFY_PPZV(&_1$$4);
+			if (zephir_require_zval_ret(&_1$$4, data TSRMLS_CC) == FAILURE) {
+				RETURN_MM_NULL();
+			}
+			ZEPHIR_CPY_WRT(data, _1$$4);
+		} else {
+			ZEPHIR_INIT_VAR(_2$$5);
+			object_init_ex(_2$$5, ice_exception_ce);
+			ZEPHIR_INIT_VAR(_3$$5);
+			zephir_create_array(_3$$5, 2, 0 TSRMLS_CC);
+			ZEPHIR_INIT_VAR(_4$$5);
+			ZVAL_STRING(_4$$5, "Config file '%s' doesn't exist", 1);
+			zephir_array_fast_append(_3$$5, _4$$5);
+			zephir_array_fast_append(_3$$5, data);
+			ZEPHIR_CALL_METHOD(NULL, _2$$5, "__construct", &_5, 13, _3$$5);
+			zephir_check_call_status();
+			zephir_throw_exception_debug(_2$$5, "ice/config.zep", 29 TSRMLS_CC);
+			ZEPHIR_MM_RESTORE();
+			return;
+		}
+	}
 	if (Z_TYPE_P(data) != IS_ARRAY) {
 		if (Z_TYPE_P(data) != IS_NULL) {
-			ZEPHIR_THROW_EXCEPTION_DEBUG_STR(ice_exception_ce, "The configuration must be an Array", "ice/config.zep", 27);
+			ZEPHIR_THROW_EXCEPTION_DEBUG_STR(ice_exception_ce, "The configuration must be an Array", "ice/config.zep", 35);
 			return;
 		} else {
 			RETURN_MM_NULL();
 		}
 	}
-	zephir_is_iterable(data, &_1, &_0, 0, 0, "ice/config.zep", 56);
+	zephir_is_iterable(data, &_7, &_6, 0, 0, "ice/config.zep", 64);
 	for (
-	  ; zend_hash_get_current_data_ex(_1, (void**) &_2, &_0) == SUCCESS
-	  ; zend_hash_move_forward_ex(_1, &_0)
+	  ; zend_hash_get_current_data_ex(_7, (void**) &_8, &_6) == SUCCESS
+	  ; zend_hash_move_forward_ex(_7, &_6)
 	) {
-		ZEPHIR_GET_HMKEY(key, _1, _0);
-		ZEPHIR_GET_HVALUE(value, _2);
+		ZEPHIR_GET_HMKEY(key, _7, _6);
+		ZEPHIR_GET_HVALUE(value, _8);
 		if (Z_TYPE_P(key) != IS_STRING) {
-			ZEPHIR_THROW_EXCEPTION_DEBUG_STR(ice_exception_ce, "Only string keys are allowed", "ice/config.zep", 35);
+			ZEPHIR_THROW_EXCEPTION_DEBUG_STR(ice_exception_ce, "Only string keys are allowed", "ice/config.zep", 43);
 			return;
 		}
 		if (Z_TYPE_P(value) == IS_ARRAY) {
 			hasNumericKey = 0;
-			zephir_is_iterable(value, &_4$$8, &_3$$8, 0, 0, "ice/config.zep", 47);
+			zephir_is_iterable(value, &_10$$11, &_9$$11, 0, 0, "ice/config.zep", 55);
 			for (
-			  ; zend_hash_get_current_data_ex(_4$$8, (void**) &_5$$8, &_3$$8) == SUCCESS
-			  ; zend_hash_move_forward_ex(_4$$8, &_3$$8)
+			  ; zend_hash_get_current_data_ex(_10$$11, (void**) &_11$$11, &_9$$11) == SUCCESS
+			  ; zend_hash_move_forward_ex(_10$$11, &_9$$11)
 			) {
-				ZEPHIR_GET_HMKEY(subkey, _4$$8, _3$$8);
-				ZEPHIR_GET_HVALUE(subvalue, _5$$8);
+				ZEPHIR_GET_HMKEY(subkey, _10$$11, _9$$11);
+				ZEPHIR_GET_HVALUE(subvalue, _11$$11);
 				if (Z_TYPE_P(subkey) == IS_LONG) {
 					hasNumericKey = 1;
 					break;
@@ -94,11 +127,11 @@ PHP_METHOD(Ice_Config, __construct) {
 			if (hasNumericKey) {
 				zephir_update_property_array(this_ptr, SL("data"), key, value TSRMLS_CC);
 			} else {
-				ZEPHIR_INIT_NVAR(_6$$12);
-				object_init_ex(_6$$12, ice_config_ce);
-				ZEPHIR_CALL_METHOD(NULL, _6$$12, "__construct", &_7, 26, value);
+				ZEPHIR_INIT_NVAR(_12$$15);
+				object_init_ex(_12$$15, ice_config_ce);
+				ZEPHIR_CALL_METHOD(NULL, _12$$15, "__construct", &_13, 26, value);
 				zephir_check_call_status();
-				zephir_update_property_array(this_ptr, SL("data"), key, _6$$12 TSRMLS_CC);
+				zephir_update_property_array(this_ptr, SL("data"), key, _12$$15 TSRMLS_CC);
 			}
 		} else {
 			zephir_update_property_array(this_ptr, SL("data"), key, value TSRMLS_CC);
