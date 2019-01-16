1f41d95aa525337b6d32935596f83c4be4b241cc
Ice Framework <info@iceframework.org>
2019-01-16 13:49:07 +0000

    php7, 06f4ce70d98d29d648f4e2a314c938bc59370877: regenerate ice sources [ci skip]

--- build/php7/php_ice.h
+++ build/php7/php_ice.h
@@ -14,7 +14,7 @@
 #define PHP_ICE_VERSION     "1.4.1"
 #define PHP_ICE_EXTNAME     "ice"
 #define PHP_ICE_AUTHOR      "Ice Team"
-#define PHP_ICE_ZEPVERSION  "0.11.8-$Id$"
+#define PHP_ICE_ZEPVERSION  "0.11.9-$Id$"
 #define PHP_ICE_DESCRIPTION "Simple and fast PHP framework delivered as C-extension.<br>Copyright (c) 2014-2018 Ice Team."
 
 
